6163cbf8fda4f98f4d0210b97b9667b0158202f1
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-05-03 16:56:56 +0100

    neuralpost blog v3.8

--- Components/FeaturedPost.jsx
+++ Components/FeaturedPost.jsx
@@ -1,14 +1,165 @@
 import React from "react";
+import Image from "next/image";
 
 const FeaturedPost = () => {
   return (
     <div>
       <div className="FeaturedPost__Container">
-        <h1>Olarewaju ode</h1>
-        <h1>Olarewaju ode</h1>
-        <h1>Olarewaju ode</h1>
-        <h1>Olarewaju ode</h1>
-        <h1>Olarewaju ode</h1>
+        <div className="FeaturedPost__Container--box">
+          <div className="FeaturedPost__Container--box--image">
+            <Image src="/banner3.jpeg" alt="post" width={1000} height={500} />
+          </div>
+          <div className="FeaturedPost__Container--box--text">
+            <h6>February 3, 2022</h6>
+            <br />
+            <h3>Breaking Gender Biases with Crypto</h3>
+            <br />
+            <p>
+              In the game of Chess, the Pawn is the least powerful character; as
+              they get to make limited moves. However, the beauty of the game is
+              that this same limited character can be promoted to become the
+              most powerful character in the game — The Queen.
+            </p>
+          </div>
+          <div className="FeaturedAuthor__info">
+            <div className="FeaturedAuthor__info--image"></div>
+            <div className="FeaturedAuthor__info--text">
+              <h6>Eran Olarewaju </h6>
+
+              <h6>Growth & Marketing</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="FeaturedPost__Container--box">
+          <div className="FeaturedPost__Container--box--image">
+            <Image src="/banner3.jpeg" alt="post" width={1000} height={500} />
+          </div>
+          <div className="FeaturedPost__Container--box--text">
+            <h6>February 3, 2022</h6>
+            <br />
+            <h3>Breaking Gender Biases with Crypto</h3>
+            <br />
+            <p>
+              In the game of Chess, the Pawn is the least powerful character; as
+              they get to make limited moves. However, the beauty of the game is
+              that this same limited character can be promoted to become the
+              most powerful character in the game — The Queen.
+            </p>
+          </div>
+          <div className="FeaturedAuthor__info">
+            <div className="FeaturedAuthor__info--image"></div>
+            <div className="FeaturedAuthor__info--text">
+              <h6>Eran Olarewaju </h6>
+
+              <h6>Growth & Marketing</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="FeaturedPost__Container--box">
+          <div className="FeaturedPost__Container--box--image">
+            <Image src="/banner3.jpeg" alt="post" width={1000} height={500} />
+          </div>
+          <div className="FeaturedPost__Container--box--text">
+            <h6>February 3, 2022</h6>
+            <br />
+            <h3>Breaking Gender Biases with Crypto</h3>
+            <br />
+            <p>
+              In the game of Chess, the Pawn is the least powerful character; as
+              they get to make limited moves. However, the beauty of the game is
+              that this same limited character can be promoted to become the
+              most powerful character in the game — The Queen.
+            </p>
+          </div>
+          <div className="FeaturedAuthor__info">
+            <div className="FeaturedAuthor__info--image"></div>
+            <div className="FeaturedAuthor__info--text">
+              <h6>Eran Olarewaju </h6>
+
+              <h6>Growth & Marketing</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="FeaturedPost__Container--box">
+          <div className="FeaturedPost__Container--box--image">
+            <Image src="/banner3.jpeg" alt="post" width={1000} height={500} />
+          </div>
+          <div className="FeaturedPost__Container--box--text">
+            <h6>February 3, 2022</h6>
+            <br />
+            <h3>Breaking Gender Biases with Crypto</h3>
+            <br />
+            <p>
+              In the game of Chess, the Pawn is the least powerful character; as
+              they get to make limited moves. However, the beauty of the game is
+              that this same limited character can be promoted to become the
+              most powerful character in the game — The Queen.
+            </p>
+          </div>
+          <div className="FeaturedAuthor__info">
+            <div className="FeaturedAuthor__info--image"></div>
+            <div className="FeaturedAuthor__info--text">
+              <h6>Eran Olarewaju </h6>
+
+              <h6>Growth & Marketing</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="FeaturedPost__Container--box">
+          <div className="FeaturedPost__Container--box--image">
+            <Image src="/banner3.jpeg" alt="post" width={1000} height={500} />
+          </div>
+          <div className="FeaturedPost__Container--box--text">
+            <h6>February 3, 2022</h6>
+            <br />
+            <h3>Breaking Gender Biases with Crypto</h3>
+            <br />
+            <p>
+              In the game of Chess, the Pawn is the least powerful character; as
+              they get to make limited moves. However, the beauty of the game is
+              that this same limited character can be promoted to become the
+              most powerful character in the game — The Queen.
+            </p>
+          </div>
+          <div className="FeaturedAuthor__info">
+            <div className="FeaturedAuthor__info--image"></div>
+            <div className="FeaturedAuthor__info--text">
+              <h6>Eran Olarewaju </h6>
+
+              <h6>Growth & Marketing</h6>
+            </div>
+          </div>
+        </div>
+
+        <div className="FeaturedPost__Container--box">
+          <div className="FeaturedPost__Container--box--image">
+            <Image src="/banner3.jpeg" alt="post" width={1000} height={500} />
+          </div>
+          <div className="FeaturedPost__Container--box--text">
+            <h6>February 3, 2022</h6>
+            <br />
+            <h3>Breaking Gender Biases with Crypto</h3>
+            <br />
+            <p>
+              In the game of Chess, the Pawn is the least powerful character; as
+              they get to make limited moves. However, the beauty of the game is
+              that this same limited character can be promoted to become the
+              most powerful character in the game — The Queen.
+            </p>
+          </div>
+          <div className="FeaturedAuthor__info">
+            <div className="FeaturedAuthor__info--image"></div>
+            <div className="FeaturedAuthor__info--text">
+              <h6>Eran Olarewaju </h6>
+
+              <h6>Growth & Marketing</h6>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
